b46a72c8de5e2289583c6dc6694a5133f11c5bc9
alelizzt <alelizzt@hotmail.com>
2019-08-21 15:52:47 -0500

    Adding raw_input to port scanner

--- scanning/portscan1.py
+++ scanning/portscan1.py
@@ -4,16 +4,18 @@ import socket
 
 #Creando un socket que utiliza ipv4 y TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.setdefaulttimeout(2)
 
-host = "127.0.0.1"
-port = 443
+#Leyendo la ip y el puerto a escanear
+host = raw_input("[*] Enter the host to scan: ")
+port = int(raw_input("[*] Enter the port to scan: "))
 
 #Definiendo una funcion para ver si el puerto esta abierto o cerrado
 def portscanner(port):
     if sock.connect_ex((host,port)):
         print "Port %d is closed" % (port)
     else:
-        print "Port %d is opened" % (port)
+        print "Port %d is open" % (port)
 
 portscanner(port)
         
