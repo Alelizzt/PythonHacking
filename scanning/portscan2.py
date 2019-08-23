0787925edca6305af49387e43accca7ae77c1d2d
alelizzt <alelizzt@hotmail.com>
2019-08-23 14:49:56 -0500

    Add some color to ports response

--- scanning/portscan2.py
+++ scanning/portscan2.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import socket
+from termcolor import colored
 
 #Creando un socket que utiliza ipv4 y TCP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.setdefaulttimeout(2)
 
 #Leyendo la ip y el puerto a escanear
-host = raw_input("[*] Enter the host to scan: ")
+host = input("[*] Enter the host to scan: ")
 
 #Definiendo una funcion para ver si el puerto esta abierto o cerrado
 def portscanner(port):
     if sock.connect_ex((host,port)):
-        print "[!!] Port %d is closed" % (port)
+        print(colored("[!!] Port %d is closed" % (port), 'red'))
     else:
-        print "[+] Port %d is open" % (port)
+        print(colored("[+] Port %d is open" % (port), 'green'))
 
 for port in range(1,100):
     portscanner(port)
