835344a833386b19c3a4b81468721d34a75d0ca3
alelizzt <alelizzt@hotmail.com>
2019-08-23 15:30:35 -0500

    upgrading script to Python3

--- scanning/advancescanner.py
+++ scanning/advancescanner.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from socket import *
 import optparse
 from threading import *
-
+from termcolor import colored
 
 def connScan(tgtHost, tgtPort):
     try:
         sock = socket(AF_INET, SOCK_STREAM)
         sock.connect(tgtHost, tgtPort)
-        print '[+] %d/tcp open' % tgtPort
+        print (colored('[+] %d/tcp open' % tgtPort, 'green'))
     except:
-        print '[-] %d/tcp closed' % tgtPort
+        print (colored('[-] %d/tcp closed' % tgtPort, 'red'))
     finally:
         sock.close()
 
@@ -19,12 +19,12 @@ def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
     except:
-        print 'Unknown Host %s ' %tgtHost
+        print (colored('Unknown Host %s ' %tgtHost, 'red'))
     try:
         tgtName = gethostbyaddr(tgtIP)
-        print '[+] Scan results for: ' + tgtName[0]
+        print ('[+] Scan results for: ' + tgtName[0])
     except:
-        print '[+] Scan results for: ' + tgtIP
+        print ('[+] Scan results for: ' + tgtIP)
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
         t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
@@ -39,7 +39,7 @@ def main():
     tgtHost = options.tgtHost
     tgtPorts = str(options.tgtPort).split(',')
     if (tgtHost == None) | (tgtPorts[0] == None):
-        print parser.usage
+        print (parser.usage)
         exit(0)
     portScan(tgtHost, tgtPorts)
 
