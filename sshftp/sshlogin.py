6b698ddb71708e00e55cc5b8c706ffd63c2cbc55
Alelizzt <alejandrolistc@hotmail.com>
2019-09-11 17:31:38 -0500

    add connect function to sshlogin script

--- sshftp/sshlogin.py
+++ sshftp/sshlogin.py
@@ -2,6 +2,24 @@
 
 import pexpect
 
+PROMPT = ['# ','>>> ','> ','\$ ']
+
+def connect(user,host,password):
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    connStr = 'ssh ' + user + '@' + host
+    child = pexpect.spawn(connStr)
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword: '])
+    if ret == 0:
+        print '[-] Error connecting'
+        return
+    if ret == 1:
+        child.sendline('yes')
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword: '])
+        if ret == 0:
+            print '[-] Error connecting'
+            return
+    child.sendline(password)
+    child.pexpect(PROMPT)
 
 
 def main():
